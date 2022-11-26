a6d0638efb0592785ab5a14bd405e4b99f5169ce
Juan <gurquiza8@gmail.com>
2022-11-25 21:10:03 -0600

    enviando parametros a las pantallas de navegacion

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { styles } from '../theme/appTheme';
 
@@ -10,10 +10,42 @@ export const Pagina1Screen = ({ navigation }: Props) => {
     return (
         <View style={ styles.globalMargin }>
             <Text style={ styles.title }>Pagina 1 Screen</Text>
+
             <Button 
                 title='Ir a página 2'
                 onPress={ () => navigation.navigate('Pagina2Screen') }
             />
+
+            <Text>Navegar con argumentos</Text>
+
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                    style={{ 
+                        ...styles.botonGrande,
+                        backgroundColor: '#5856D6'
+                     }}
+                    onPress={ () => navigation.navigate('PersonaScreen', {
+                        id: '1',
+                        nombre: 'Peter'
+                    }) }
+                >
+                    <Text style={ styles.botonGrandeTexto } >Peter</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        ...styles.botonGrande,
+                        backgroundColor: '#FF9437'
+                    }}
+                    onPress={ () => navigation.navigate('PersonaScreen', {
+                        id: '2',
+                        nombre: 'Maria'
+                    }) }
+                    >
+                    <Text style={ styles.botonGrandeTexto } >Maria</Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
